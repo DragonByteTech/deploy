155684d72ba2784d245da2d2bfc03a64ab5144ad
Fillip H <fillip@dragonbyte-tech.com>
2019-09-11 16:07:20 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM debian:stable-20190122-slim
+
+LABEL "com.github.actions.name"="Rsync Deploy"
+LABEL "com.github.actions.description"="Deploy to a remote server with rsync via ssh."
+LABEL "com.github.actions.icon"="upload-cloud"
+LABEL "com.github.actions.color"="orange"
+
+RUN apt-get update && \ 
+    apt-get install -y \ 
+    openssh-client \ 
+    wget \ 
+    curl \ 
+    php \ 
+    git \ 
+    zip \ 
+    gpg \ 
+    rsync && \ 
+    rm -rf /var/lib/apt/lists/*
+
+ENV JQ_VERSION='1.5'
+
+RUN wget --no-check-certificate https://raw.githubusercontent.com/stedolan/jq/master/sig/jq-release.key -O /tmp/jq-release.key && \
+    wget --no-check-certificate https://raw.githubusercontent.com/stedolan/jq/master/sig/v${JQ_VERSION}/jq-linux64.asc -O /tmp/jq-linux64.asc && \
+    wget --no-check-certificate https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64 -O /tmp/jq-linux64 && \
+    gpg --import /tmp/jq-release.key && \
+    gpg --verify /tmp/jq-linux64.asc /tmp/jq-linux64 && \
+    cp /tmp/jq-linux64 /usr/bin/jq && \
+    chmod +x /usr/bin/jq && \
+    rm -f /tmp/jq-release.key && \
+    rm -f /tmp/jq-linux64.asc && \
+    rm -f /tmp/jq-linux64
+
+RUN curl -sS https://getcomposer.org/installer | \
+            php -- --install-dir=/usr/bin/ --filename=composer
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit  # same as -e
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+
+SSH_USER=dbtech
+HOST_NAME=dragonbyte-tech.com
+
+if [[ $BRANCH =~ ^refs/tags/.* ]];
+then
+	version=$(echo "$BRANCH" | sed 's#refs/tags/##')
+
+	# Now update the version variable with the hotfix removed as we don't want that going forward
+	version=$(echo "$version" | sed 's#[-hotfix].*$##')
+
+else
+	branch=`echo $BRANCH | cut -d/ -f3`
+	version=`echo $BRANCH | cut -d/ -f4`
+
+	if [[ $branch == hotfix ]];
+	then
+		# Now update the version variable with the hotfix removed as we don't want that going forward
+		version=$(echo "$version" | sed 's#[-hotfix].*$##')
+	fi
+fi
+
+if [ -z "$version" ];
+then
+  echo "No valid version found." >&2
+  exit 1
+fi
+
+cd ..
+
+mkdir deployment
+
+if [ ! -z "$ADDON_ROOT" ];
+then
+	releaseDir="vbec_products/$ADDON_DIR/$version"
+	addonDir="$releaseDir/$ADDON_ROOT"
+
+	mkdir -p deployment/$ADDON_ROOT
+	cp -R workspace/* deployment/$ADDON_ROOT
+
+	if [ -d deployment/$ADDON_ROOT/_files ]; then
+		cp -R deployment/$ADDON_ROOT/_files/* deployment/upload
+		rm -rf deployment/$ADDON_ROOT/_files
+	fi
+	
+	if [ -d deployment/$ADDON_ROOT/_no_upload ]; then
+		cp -R deployment/$ADDON_ROOT/_no_upload/* deployment
+		rm -rf deployment/$ADDON_ROOT/_no_upload
+	fi
+
+	if [ -f deployment/$ADDON_ROOT/build-server.json ]; then
+		cd deployment
+
+		while read -r cmd; do
+			sh -c "${cmd}"
+		done < <(cat $ADDON_ROOT/build-server.json | jq --raw-output '.exec[]')
+
+		cd ..
+	fi
+
+	rm -rf deployment/$ADDON_ROOT/.git
+	rm -rf deployment/$ADDON_ROOT/.github
+	rm -rf deployment/$ADDON_ROOT/_output
+
+else
+	releaseDir="vbec_products/$ADDON_DIR/$version"
+	cp -R workspace/* deployment
+
+	rm -rf "deployment/.git"
+	rm -rf "deployment/.github"
+fi
+
+
+printf -- 'Setting up SSH... '
+
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
+
+mkdir /root/.ssh
+touch /root/.ssh/known_hosts
+
+echo "$SSH_PRIVATE_KEY" > /root/.ssh/deploy_key
+echo "$SSH_PUBLIC_KEY" > /root/.ssh/deploy_key.pub
+
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/deploy_key
+echo "$SSH_PUBLIC_KEY" > ~/.ssh/deploy_key.pub
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/known_hosts
+chmod 600 ~/.ssh/deploy_key
+chmod 600 ~/.ssh/deploy_key.pub
+
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/known_hosts
+chmod 600 /root/.ssh/deploy_key
+chmod 600 /root/.ssh/deploy_key.pub
+
+
+printf -- 'Recording known host... '
+
+ssh-keyscan $HOST_NAME >> ~/.ssh/known_hosts
+ssh-keyscan $HOST_NAME >> /root/.ssh/known_hosts
+
+
+printf -- 'Adding deploy key... '
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/deploy_key
+
+
+# $HOST_NAME is used in the above as well as in the below; that's why it is an env
+
+printf -- 'Cleaning up old deployment... '
+sh -c "ssh $SSH_USER@$HOST_NAME 'rm -rf $releaseDir && mkdir -p $releaseDir'"
+
+
+printf -- 'Deploying project... '
+sh -c "rsync --progress --verbose --recursive --delete-after --quiet deployment/ $SSH_USER@$HOST_NAME:$releaseDir"
+
+
+
+printf -- '\033[32m Deployment successful! \033[0m\n'
+printf -- '\n'
